ff54c770587207bc4470397fd631c0c406da20f0
Anna <anna@Noutbuk-Anna.local>
2023-04-14 13:51:30 +0300

    solve Dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let arr = members;
+  let newArr = [];
+  //Проверяем приходит ли нам array
+  if (!Array.isArray(arr)){
+    return false;
+  }
+  //Возвращаем элементы в массиве только с типом string
+  arr = arr.filter((item) => {
+    return typeof item === 'string'
+  });
+//Перебираем, удаляем пробелы, отрезаем 1ую букву, делаем ее заглавной, добавляем в новый массив
+  for(let i = 0; i < arr.length; i++){
+      let char = arr[i].trim().slice(0,1).toUpperCase();
+      newArr.push(char);
+  }
+  //сортируем новый массив по алфавиту, соединяем в строку
+let result = newArr.sort().join('');
+return result;
 }
 
 module.exports = {
