cc9c5d02ac712c3986f7de41adb0740301a7e658
annaAzh <forarre@yandex.ru>
2023-12-04 09:47:40 +0300

    feat: solved dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,24 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function createDreamTeam(members) {
-  let arr = members;
-  let newArr = [];
-  //Проверяем приходит ли нам array
-  if (!Array.isArray(arr)){
+  if (!Array.isArray(members)){
     return false;
   }
-  //Возвращаем элементы в массиве только с типом string
-  arr = arr.filter((item) => {
-    return typeof item === 'string'
-  });
-//Перебираем, удаляем пробелы, отрезаем 1ую букву, делаем ее заглавной, добавляем в новый массив
-  for(let i = 0; i < arr.length; i++){
-      let char = arr[i].trim().slice(0,1).toUpperCase();
-      newArr.push(char);
-  }
-  //сортируем новый массив по алфавиту, соединяем в строку
-let result = newArr.sort().join('');
-return result;
+  return members.
+  filter(elem => typeof elem === 'string').
+  map(elem => elem.trim().slice(0, 1).toUpperCase()).
+  sort().
+  join('');
 }
 
 module.exports = {
