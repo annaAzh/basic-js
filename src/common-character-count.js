eb1e1edb0323b12725ef43bf3227cf09535352f5
annaAzh <forarre@yandex.ru>
2023-12-04 13:25:13 +0300

    feat: solve common-character-count task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,18 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  let count = 0;
-  s1 = s1.split('');
-  s2 = s2.split('');
-
-  s1.forEach((elem)=> {
-    if(s2.includes(elem)) {
-      count++;
-      s2.splice(s2.indexOf(elem),1);
+  const length = s1.length > s2.length ? s1.length : s2.length;
+  const arr1 = s1.split('');
+  const arr2 = s2.split('');
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    if (arr2.indexOf(arr1[i]) > -1) {
+      const index = arr2.indexOf(arr1[i]);
+      arr.push(arr1[i]);
+      arr2.splice(index, 1)
     }
-  })
-
-  return count;
+  }
+  return arr.length;
 }
 
 module.exports = {
