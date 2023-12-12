e99f8e010681c251fb8871ee5ad5793c9fbf7ca9
annaAzh <forarre@yandex.ru>
2023-12-12 19:24:40 +0300

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/*sampleActivity*/) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!Number(sampleActivity) || typeof sampleActivity !== 'string') {
+    return false;
+  } 
+  const constant = 0.693;
+  const k =  constant / HALF_LIFE_PERIOD;
+  const result =  Math.ceil(Math.log( MODERN_ACTIVITY / +sampleActivity) / k);
+  return result > 0 ? result : false;
 }
 
 module.exports = {
