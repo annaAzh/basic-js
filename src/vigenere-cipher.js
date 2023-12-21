ccafd2cf52dc2d8cd38fc38f70223f50111ba0a0
annaAzh <forarre@yandex.ru>
2023-12-21 10:53:55 +0300

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,87 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.char = ':1234567890!@#$(),./|*-&^% ';
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if (typeof message === 'undefined' || typeof key === 'undefined') {
+      throw Error('Incorrect arguments!');
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let newKeyWord = '';
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (message[i] !== ' ') {
+        newKeyWord += key[j];
+        j += 1;
+      } else 
+      newKeyWord += ' ';
+      if (j === key.length) {
+        j = 0;
+      }
+    }
+
+    let result = '';
+
+    for (let i = 0, j = 0; i < message.length; i += 1) {
+      if (this.char.indexOf(message[i]) > -1) {
+        result += message[i];
+        j++;
+        continue;
+      } else {
+        let a = this.alphabet.indexOf(message[i]);
+        let b = this.alphabet.indexOf(newKeyWord[j]);
+        let charIndex =  a + b < this.alphabet.length ? a + b : (a + b) - this.alphabet.length;
+        result += this.alphabet[charIndex];
+      }
+      j++;
+    }
+    if (!this.isDirect) {
+      return result.split('').reverse().join('');
+    }
+    return result;
+  }
+
+  decrypt(message, key) {
+    if (typeof message === 'undefined' || typeof key === 'undefined') {
+      throw Error('Incorrect arguments!');
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let newKeyWord = '';
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (message[i] !== ' ') {
+        newKeyWord += key[j];
+        j += 1;
+      } else 
+      newKeyWord += ' ';
+      if (j === key.length) {
+        j = 0;
+      }
+    }
+    let result = '';
+
+    for (let i = 0, j = 0; i < message.length; i += 1) {
+      if (this.char.indexOf(message[i]) > -1) {
+        result += message[i];
+        j++;
+        continue;
+      } else {
+        let a = this.alphabet.indexOf(message[i]);
+        let b = this.alphabet.indexOf(newKeyWord[j]);
+        let charIndex = (a - b) < 0 ? a - b + this.alphabet.length : (a - b);
+        result += this.alphabet[charIndex];
+      }
+      j++;
+    }
+    if (!this.isDirect) {
+      return result.split('').reverse().join('');
+    }
+    return result;
   }
 }
 
